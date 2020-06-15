f56bdfb509e9374a4bf5b5692fd0e1856394f19b
DesmondTambe <desking2000@gmail.com>
2020-06-15 12:25:15 -0700

    Investment Opportunities Screen completion

--- screens/OppotunitiesScreen.js
+++ screens/OppotunitiesScreen.js
@@ -1,12 +1,27 @@
 import * as React from "react";
-import { Text } from "react-native";
+import { Text, StatusBar, Platform } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Progress from "../components/samples/progressBar";
+import { ScrollView } from "react-native-gesture-handler";
+import { OppotunityPreview } from "../components/OppotunityPreview";
 
 export default function OppotunitiesScreen() {
   return (
     <SafeAreaView>
-      <Progress></Progress>
+      <Text
+        style={{
+          textAlign: "center",
+          padding: 20,
+          backgroundColor: "#FFFFFF",
+          fontSize: 23,
+          fontWeight: "bold",
+        }}
+      >
+        Invesments Opportunities
+      </Text>
+      <ScrollView>
+        <OppotunityPreview />
+      </ScrollView>
     </SafeAreaView>
   );
 }
