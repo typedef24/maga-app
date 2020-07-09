2537aa8c443cd4944729b8a574220b0ae5e604a2
typedef24 <nembosteph@gmail.com>
2020-07-09 22:52:31 +0100

    Compare Investments Logic Done and now redirects to Compare table

--- screens/CompareInvestmentsScreen.js
+++ screens/CompareInvestmentsScreen.js
@@ -8,14 +8,14 @@ import {
   Alert,
 } from "react-native";
 import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
-
+import { MaterialIcons } from "@expo/vector-icons";
 import CompareInvestmentsComponent from "../components/CompareInvestment";
 import fonts from "../constants/fonts";
 import AppConstants from "../constants/AppConstants";
 
 import img from "../assets/images/dish.png";
 
-export default function CompareInvestmentsScreen({ route }) {
+export default function CompareInvestmentsScreen({ route, navigation }) {
   const data = route.params.data;
 
   const [compareArray, setCompareArray] = useState([]);
@@ -31,11 +31,27 @@ export default function CompareInvestmentsScreen({ route }) {
             onClickFnx={() => {
               //ensure compareArray has less than 4 items
               if (compareArray.length < 4) {
-                let tempCompareArray = compareArray.slice(); //copy compareArray to a temporal array
-                tempCompareArray.push(item); //add new item to tempCompareArray
-                setCompareArray(tempCompareArray);
-                console.log("Compare Array: " + JSON.stringify(compareArray));
-                console.log("Item added to state: " + JSON.stringify(item));
+                //ensure that item doesn't already exist in compareArray
+                let isItemInCompareArray = false;
+                for (let index = 0; index < compareArray.length; index++) {
+                  if (compareArray[index].id == item.id) {
+                    //item already in compareArray
+                    isItemInCompareArray = true;
+                    break;
+                  }
+                }
+                if (!isItemInCompareArray) {
+                  let tempCompareArray = compareArray.slice(); //copy compareArray to a temporal array
+                  tempCompareArray.push(item); //add new item to tempCompareArray
+                  setCompareArray(tempCompareArray);
+                  console.log("Compare Array: " + JSON.stringify(compareArray));
+                  console.log("Item added to state: " + JSON.stringify(item));
+                } else {
+                  Alert.alert(
+                    "NOTICE!",
+                    "Investment already in your compare list."
+                  );
+                }
               } else {
                 Alert.alert(
                   "NOTICE!",
@@ -48,50 +64,106 @@ export default function CompareInvestmentsScreen({ route }) {
       />
       <View style={styles.container}>
         <View style={styles.boxContainer}>
-          <TouchableOpacity style={styles.boxSimple}>
-            {compareArray[0] && (
-              <Image
-                source={{
-                  uri: AppConstants.strapiBaseURL + compareArray[0].img.url,
-                }}
-                style={{ height: 30, width: 30 }}
-                resizeMode="cover"
-              />
+          <View style={styles.boxSimple}>
+            {compareArray[0] ? (
+              <View style={{ flex: 1 }}>
+                <TouchableOpacity
+                  style={{ marginRight: -11, marginTop: -11, alignItems: "flex-end" }}
+                  onPress={() => {
+                    let tempCompareArray = compareArray.slice(); //copy compareArray to a temporal array
+                    tempCompareArray.splice(0, 1); //Remove the item at index 0
+                    setCompareArray(tempCompareArray);
+                  }}
+                >
+                  <MaterialIcons name="cancel" size={25} color="red" />
+                </TouchableOpacity>
+                <Image
+                  source={{
+                    uri: AppConstants.strapiBaseURL + compareArray[0].img.url,
+                  }}
+                  style={{ height: 30, width: 30, marginTop: -10 }}
+                  resizeMode="cover"
+                />
+              </View>
+            ) : (
+              <View style={{ height: 30, width: 30 }}></View>
             )}
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.boxSimple}>
-            {compareArray[1] && (
-              <Image
-                source={{
-                  uri: AppConstants.strapiBaseURL + compareArray[1].img.url,
-                }}
-                style={{ height: 30, width: 30 }}
-                resizeMode="cover"
-              />
+          </View>
+          <View style={styles.boxSimple}>
+            {compareArray[1] ? (
+              <View style={{ flex: 1 }}>
+                <TouchableOpacity
+                  style={{ marginRight: -11, marginTop: -11, alignItems: "flex-end" }}
+                  onPress={() => {
+                    let tempCompareArray = compareArray.slice(); //copy compareArray to a temporal array
+                    tempCompareArray.splice(1, 1); //Remove the item at index 1
+                    setCompareArray(tempCompareArray);
+                  }}
+                >
+                  <MaterialIcons name="cancel" size={25} color="red" />
+                </TouchableOpacity>
+                <Image
+                  source={{
+                    uri: AppConstants.strapiBaseURL + compareArray[0].img.url,
+                  }}
+                  style={{ height: 30, width: 30, marginTop: -10 }}
+                  resizeMode="cover"
+                />
+              </View>
+            ) : (
+              <View style={{ height: 30, width: 30 }}></View>
             )}
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.boxSimple}>
-            {compareArray[2] && (
-              <Image
-                source={{
-                  uri: AppConstants.strapiBaseURL + compareArray[2].img.url,
-                }}
-                style={{ height: 30, width: 30 }}
-                resizeMode="cover"
-              />
+          </View>
+          <View style={styles.boxSimple}>
+            {compareArray[2] ? (
+              <View style={{ flex: 1 }}>
+                <TouchableOpacity
+                  style={{ marginRight: -11, marginTop: -11, alignItems: "flex-end" }}
+                  onPress={() => {
+                    let tempCompareArray = compareArray.slice(); //copy compareArray to a temporal array
+                    tempCompareArray.splice(2, 1); //Remove the item at index 0
+                    setCompareArray(tempCompareArray);
+                  }}
+                >
+                  <MaterialIcons name="cancel" size={25} color="red" />
+                </TouchableOpacity>
+                <Image
+                  source={{
+                    uri: AppConstants.strapiBaseURL + compareArray[0].img.url,
+                  }}
+                  style={{ height: 30, width: 30, marginTop: -10 }}
+                  resizeMode="cover"
+                />
+              </View>
+            ) : (
+              <View style={{ height: 30, width: 30 }}></View>
             )}
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.boxSimple}>
-            {compareArray[3] && (
-              <Image
-                source={{
-                  uri: AppConstants.strapiBaseURL + compareArray[3].img.url,
-                }}
-                style={{ height: 30, width: 30 }}
-                resizeMode="cover"
-              />
+          </View>
+          <View style={styles.boxSimple}>
+            {compareArray[3] ? (
+              <View style={{ flex: 1 }}>
+                <TouchableOpacity
+                  style={{ marginRight: -11, marginTop: -11, alignItems: "flex-end" }}
+                  onPress={() => {
+                    let tempCompareArray = compareArray.slice(); //copy compareArray to a temporal array
+                    tempCompareArray.splice(3, 1); //Remove the item at index 0
+                    setCompareArray(tempCompareArray);
+                  }}
+                >
+                  <MaterialIcons name="cancel" size={25} color="red" />
+                </TouchableOpacity>
+                <Image
+                  source={{
+                    uri: AppConstants.strapiBaseURL + compareArray[0].img.url,
+                  }}
+                  style={{ height: 30, width: 30, marginTop: -10 }}
+                  resizeMode="cover"
+                />
+              </View>
+            ) : (
+              <View style={{ height: 30, width: 30 }}></View>
             )}
-          </TouchableOpacity>
+          </View>
         </View>
         <View>
           <TouchableOpacity
@@ -102,6 +174,9 @@ export default function CompareInvestmentsScreen({ route }) {
               alignItems: "center",
               marginTop: 15,
               marginLeft: 5,
+            }}
+            onPress = {() => {
+              navigation.navigate("compare-table", { data: compareArray });
             }}
           >
             <Text
